023cb86485c1f59f595e07eb4961ecfbce8028ab
MrHawkins-Babington <95847296+MrHawkins-Babington@users.noreply.github.com>
2021-12-10 12:57:43 +0000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+#year 10 - functions
+
+def calcDouble(amount):
+    amount = 2 * amount
+    return amount
+
+question = #replace this with a number
+answer = calcDouble(question)
+print("Double",question,"is",answer)
+
+
+
+
+
+#Test your code and make sure it is executed correctly
+#in the space under return amount and above question -
+    ###create your own function calcHalf
+    ###create another function called calcAddTen
+    ###Create another function of your own design
